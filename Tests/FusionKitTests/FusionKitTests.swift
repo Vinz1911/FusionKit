b865e2af77a071c65a59c2c841d526d097ac946d
Vinzenz Weist <vinz@MacBook-Pro-16.fritz.box>
2023-04-02 16:12:52 +0200

    [M] added missing header to test

--- Tests/FusionKitTests/FusionKitTests.swift
+++ Tests/FusionKitTests/FusionKitTests.swift
@@ -1,3 +1,11 @@
+//
+//  FusionKitTests.swift.swift
+//  FusionKit
+//
+//  Created by Vinzenz Weist on 07.06.21.
+//  Copyright © 2021 Vinzenz Weist. All rights reserved.
+//
+
 import XCTest
 @testable import FusionKit
 
