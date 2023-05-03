94945cda1a7e747d566f833c2a51820a7069a2cf
Vinzenz Weist <Vinz1911@gmail.com>
2023-05-04 01:28:23 +0200

    [B] removed broken property wrapper

--- Sources/FusionKit/Model/FNConnectionFrame.swift
+++ Sources/FusionKit/Model/FNConnectionFrame.swift
@@ -10,8 +10,8 @@ import Foundation
 import CryptoKit
 
 internal final class FNConnectionFrame: FNConnectionFrameProtocol {
-    private var buffer = Atomic<Data>(.init())
-    internal func reset() { buffer.mutate { $0 = Data() } }
+    private var buffer = Data()
+    internal func reset() { buffer.removeAll() }
     
     /// create a protocol conform message frame
     /// - Parameter message: generic type which conforms to 'Data' and 'String'
@@ -31,19 +31,19 @@ internal final class FNConnectionFrame: FNConnectionFrameProtocol {
     ///   - data: the data which should be parsed
     ///   - completion: completion block returns parsed message
     internal func parse(data: Data, _ completion: (FNConnectionMessage?, Error?) -> Void) -> Void {
-        buffer.mutate { $0.append(data) }
+        buffer.append(data)
         guard let length = extractSize() else { return }
-        guard buffer.value.count <= FNConnectionCounts.frame.rawValue else { completion(nil, FNConnectionFrameError.readBufferOverflow); return }
-        guard buffer.value.count >= FNConnectionCounts.overhead.rawValue, buffer.value.count >= length else { return }
-        while buffer.value.count >= length && length != .zero {
-            guard SHA256.hash(data: buffer.value.prefix(FNConnectionCounts.control.rawValue)) == extractHash() else { completion(nil, FNConnectionFrameError.hashMismatch); return }
+        guard buffer.count <= FNConnectionCounts.frame.rawValue else { completion(nil, FNConnectionFrameError.readBufferOverflow); return }
+        guard buffer.count >= FNConnectionCounts.overhead.rawValue, buffer.count >= length else { return }
+        while buffer.count >= length && length != .zero {
+            guard SHA256.hash(data: buffer.prefix(FNConnectionCounts.control.rawValue)) == extractHash() else { completion(nil, FNConnectionFrameError.hashMismatch); return }
             guard let bytes = extractMessage() else { completion(nil, FNConnectionFrameError.parsingFailed); return }
-            switch buffer.value.first {
+            switch buffer.first {
             case FNConnectionOpcodes.binary.rawValue: completion(bytes, nil)
             case FNConnectionOpcodes.ping.rawValue: completion(UInt16(bytes.count), nil)
             case FNConnectionOpcodes.text.rawValue: guard let result = String(bytes: bytes, encoding: .utf8) else { return }; completion(result, nil)
             default: completion(nil, FNConnectionFrameError.parsingFailed) }
-            if buffer.value.count <= length { buffer.mutate { $0 = Data() } } else { buffer.mutate { $0 = Data(buffer.value[length...]) } }
+            if buffer.count <= length { buffer.removeAll() } else { buffer = Data(buffer[length...]) }
         }
     }
 }
@@ -54,25 +54,25 @@ private extension FNConnectionFrame {
     // extract the message hash from the data
     // if not possible it returns nil
     private func extractHash() -> SHA256Digest? {
-        guard buffer.value.count >= FNConnectionCounts.overhead.rawValue else { return nil }
-        let hash = buffer.value.subdata(in: FNConnectionCounts.control.rawValue..<FNConnectionCounts.overhead.rawValue).withUnsafeBytes { $0.load(as: SHA256.Digest.self) }
+        guard buffer.count >= FNConnectionCounts.overhead.rawValue else { return nil }
+        let hash = buffer.subdata(in: FNConnectionCounts.control.rawValue..<FNConnectionCounts.overhead.rawValue).withUnsafeBytes { $0.load(as: SHA256.Digest.self) }
         return hash
     }
     
     // extract the message frame size from the data
     // if not possible it returns nil
     private func extractSize() -> UInt32? {
-        guard buffer.value.count >= FNConnectionCounts.overhead.rawValue else { return nil }
-        let size = buffer.value.subdata(in: FNConnectionCounts.opcode.rawValue..<FNConnectionCounts.control.rawValue)
+        guard buffer.count >= FNConnectionCounts.overhead.rawValue else { return nil }
+        let size = buffer.subdata(in: FNConnectionCounts.opcode.rawValue..<FNConnectionCounts.control.rawValue)
         return size.bigEndian
     }
     
     // extract the message and remove the overhead
     // if not possible it returns nil
     private func extractMessage() -> Data? {
-        guard buffer.value.count >= FNConnectionCounts.overhead.rawValue else { return nil }
+        guard buffer.count >= FNConnectionCounts.overhead.rawValue else { return nil }
         guard let length = extractSize() else { return nil }
         guard length > FNConnectionCounts.overhead.rawValue else { return Data() }
-        return buffer.value.subdata(in: FNConnectionCounts.overhead.rawValue..<Int(length))
+        return buffer.subdata(in: FNConnectionCounts.overhead.rawValue..<Int(length))
     }
 }
